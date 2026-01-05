6815c7ced45a3029dedc9434cdd9edf164633f3f
Sujan Kumar M <sujankumar2401@gmail.com>
2026-01-05 19:22:51 +0530

    Add files via upload

---
+++ UserInput.java
@@ -0,0 +1,15 @@
+package homelayout;
+
+public class UserInput {
+
+    int familySize;
+    String budget;
+    int plotSize;
+
+    public UserInput(int familySize, String budget, int plotSize) {
+        this.familySize = familySize;
+        this.budget = budget;
+        this.plotSize = plotSize;
+    }
+}
+
